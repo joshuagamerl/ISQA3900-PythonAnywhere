b42ed19a666d74ff90182c4f9fc53fc65b379135
joshuagamerl <111942668+joshuagamerl@users.noreply.github.com>
2022-10-25 00:05:41 -0500

    Updating Settings.py to have debug be false and allowed hosts for pythonanywhere

--- GroupProject/settings.py
+++ GroupProject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1$74s6go)(5do==gjb@2x3kn6su-pp!3nz)dlmku5ln0b%q(=r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
